32a5d03de434ba90e74265b45a237641f951b117
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-11-17 15:31:51 +0600

    changes

--- StudnetManagement/src/main/java/com/StudentManagement/repository/StudentRepository.java
+++ StudnetManagement/src/main/java/com/StudentManagement/repository/StudentRepository.java
@@ -3,7 +3,6 @@ package com.StudentManagement.repository;
 import com.StudentManagement.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
